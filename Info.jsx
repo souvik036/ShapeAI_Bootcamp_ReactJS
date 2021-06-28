78b06ff458843877e78df898b1cb172d12893673
souvik036 <81374102+souvik036@users.noreply.github.com>
2021-06-28 17:55:40 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div className="note">
+      <h1>React JS</h1>
+      <p>A web dev bootcamp on Javascript and React JS</p>
+    </div>
+  );
+}
+export default Info;
